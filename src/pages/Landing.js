0f6bb94b62d786ec200c53cd728edbab54e2bc17
Matias Sfer <matias0developer@gmail.com>
2023-05-30 15:50:30 -0300

    landing page created

---
+++ src/pages/Landing.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+const Wrapper = styled.main`
+nav{
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+
+}
+.page{
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+}
+h1{
+    font-weight: 700;
+    span{
+        color:var(--primary-500);
+    }
+}
+p{
+    color: var(--grey-600);
+}
+.main-img{
+    display: none;
+}
+@media (min-width: 992px) {
+    .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap: 3rem;
+    }
+    .main-img{
+        display: block;
+    }
+}
+
+`
+
+export const Landing = () => {
+    return (
+        <Wrapper>
+            <main>
+                <nav>
+                    <img src={logo} alt='jobster logo' className='logo' />
+                </nav>
+                <div className='container page'>
+                    <div className='info'>
+                        <h1>
+                            job <span>tracking</span> app
+                        </h1>
+                        <p>Lorem ipsum dolor sit ameteget ac dui. Ins a nec eleifend iaculis, nulla metus condimentum erat, interdum congue lorem augue sit amet sapien.</p>
+                        <button className='btn btn-hero'>Login/Register</button>
+                    </div>
+                    <img src={main} alt="job hunt" className='img main-img' />
+
+                </div>
+            </main>
+        </Wrapper>
+    );
+}
